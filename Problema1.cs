675578dae5bd6da2a1254bd723be96c532981fce
Carlos <cvirgili438@gmail.com>
2023-04-25 16:44:06 -0300

    Agregar archivos de proyecto.

---
+++ Problema1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Problema1
+    {
+       
+        public Problema1() 
+        {
+           void Selected() {
+                try
+                {
+                    Console.Write("Ingrese el primer número: ");
+                    int first = int.Parse(Console.ReadLine());
+                    Console.Write("Ingrese el segundo número: ");
+                    int second = int.Parse(Console.ReadLine());
+                    bool exact = first > second;
+                    switch (exact)
+                    {
+                        case true:
+                            Console.WriteLine(first);
+                            return;
+                        case false:
+                            Console.WriteLine(second);
+                            return;
+                        default:
+                            Console.WriteLine("Como son iguales, te tiro cualquiera los 2 :" + first + " y " + second);
+                            return;
+                    }
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Por favor escriba un numero: ");
+                    Selected();
+                }
+                finally {
+                    Console.Write("Desea reiniciar el programa ? Y/N ");
+                    string response = Console.ReadLine();
+                    if (response.ToUpper() == "N")
+                    {
+                        Environment.Exit(0);
+                    }
+                    else {
+                        Program.RestartApplication();
+                    }
+                }
+            }
+            Selected();
+
+        }
+     
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    enum Choosed { 
+        Problema1= 1,
+        Problema2=2, 
+        Problema3=3, 
+        Problema4=4,
+    }
+    internal class Program
+    {
+        static string Problem1 = "1) Permitir al usuario escribir dos números" +
+            " y mostrar en pantalla cuál de los dos es el más grande. " +
+            "Si soniguales, mostrar cualquiera";
+        static string Problem2 = "2) Escribir un método que acepte un número " +
+            "y devuelva True si el número es par, o false si es impar";
+        static string Problem3 = "3) Escribir un método que acepte dos strings " +
+            "y devuelva el string con más caracteres. Si ambos tienen lamisma" +
+            " cantidad de caracteres, devolver cualquiera de los dos.";
+        static string Problem4 = "4) Escribir un método que acepte dos catetos " +
+            "y devuelve la hipotenusa (usar la clase Math para calcularla raíz cuadrada)";
+      
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("A Continuacion se " +
+                "detallaran los problemas, Elija un numero y elija " +
+                "cual quiere ejecutar: ");
+            string[] Problems = {Problem1, Problem2, Problem3,Problem4};
+            foreach (string problem in Problems) {
+                Console.WriteLine(problem);
+            }
+            
+             void ChooseProblem() {
+                try
+                {
+                    int ProblemNum = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+                    switch (ProblemNum) {
+                        case (int) Choosed.Problema1:
+                            Console.WriteLine(ProblemNum);
+                            Problema1 problem1 = new Problema1();
+                            return  ;
+                        default:
+                            Console.WriteLine("El numero que eligió, no es un ejercicio valido, elija otro :");
+                             ChooseProblem() ;
+                            return;
+
+                    }
+
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Tipee un numero, no un caracter diferente");
+                    ChooseProblem();
+                }
+            }
+            ChooseProblem();
+           
+
+        }
+        public static void RestartApplication()
+        {
+            Main(new string[] { });
+        }
+    }
+}
